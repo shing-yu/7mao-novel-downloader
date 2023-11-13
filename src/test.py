36fbaf9eec8cad73b2199ea8a6aada67e07c5781
shingyu <git@x1v.icu>
2023-11-13 14:45:40 +0800

    test(Qimao): 测试

--- src/test.py
+++ src/test.py
@@ -1,62 +1,9 @@
+import hashlib
 import public as p
-import requests
-from bs4 import BeautifulSoup
-import re
 
-# 初始化“ua”
-ua = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-    "AppleWebKit/537.36 (KHTML, like Gecko) "
-    "Chrome/118.0.0.0 "
-    "Safari/537.36"
-)
-
-headers = {
-    "User-Agent": ua
-}
-
-# 获取网页源码
-response = requests.get('https://www.qimao.com/shuku/222767/', headers=headers)
-html = response.text
-print(html)
-
-# 解析网页源码
-soup = BeautifulSoup(html, "html.parser")
-
-# 获取小说标题
-title = soup.find('div', {'class': 'title clearfix'}).find('span', {'class': 'txt'}).text
-# , class_ = "info-name"
-# 替换非法字符
-title = p.rename(title)
-
-# print(title)
-info_div = soup.find('div', class_='wrap-txt')
-
-# 在每个div标签中，找到类为'btns-wrap clearfix'的div标签
-btn = info_div.find('div', class_='btns-wrap clearfix')
-
-# 如果找到了btn标签，就从原div标签中移除它
-if btn is not None:
-    btn.extract()
-
-# 获取剩余的文本
-info_text = info_div.get_text()
-
-# 使用re模块的sub函数，将text中的多个连续的空格替换为一个空格
-info_text = re.sub(r'\s+', ' ', info_text)
-
-info = info_text + '\n'
-
-# print(info)
-
-intro = soup.find('p', class_='intro').get_text().replace(' ', '\n')
-# print(intro)
-
-# 使用正则表达式匹配类名
-# pattern = re.compile('clearfix ref-catalog-li.*')
-# clearfix ref-catalog-li-16480917400001
-li_tags = soup.find_all('li', {'class': 'clearfix ref-catalog-li-16480917400001'})
-
-# 打印匹配到的li标签
-for li in li_tags:
-    print(str(li))
+book_id = '1432205_0'
+chapter_id = '27906684'
+param_string = f"chapterId={chapter_id}id={book_id}{p.sign_key}"
+sign = hashlib.md5(param_string.encode()).hexdigest()
+encrypted_content = p.get_qimao(book_id, chapter_id, sign)
+print(encrypted_content)
