175d21d30d55ad4ef4affb0279aaf54dc5744cc6
shingyu <git@x1v.icu>
2023-11-13 21:50:19 +0800

    chore(gitignore): 忽略测试文件

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-import public as p
-
-book_id = '1432205_0'
-chapter_id = '27906684'
-param_string = f"chapterId={chapter_id}id={book_id}{p.sign_key}"
-sign = hashlib.md5(param_string.encode()).hexdigest()
-encrypted_content = p.get_qimao(book_id, chapter_id, sign)
-print(encrypted_content)
